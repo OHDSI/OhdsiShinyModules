6dce24b1cd97dba0aeaaee0c1de14d1a481065b3
Jamie Gilbert <JGilber2@its.jnj.com>
2023-07-07 13:28:35 -0700

    Tests and fully operational tables

--- R/components-largeTableViewer.R
+++ R/components-largeTableViewer.R
@@ -41,19 +41,17 @@ LargeDataTable <- R6::R6Class(
       checkmate::assertString(countQuery, null.ok = TRUE)
       checkmate::assertList(columnDefs, null.ok = TRUE, types = "colDef")
       # Cannot use multiple statments in a base query
-      stopifnot(length(strsplit(baseQuery, ";")) == 1)
+      stopifnot(length(strsplit(baseQuery, ";")[[1]]) == 1)
       self$connectionHandler <- connectionHandler
       self$baseQuery <- baseQuery
       self$columnDefs <- columnDefs
 
       if (!is.null(countQuery)) {
+        stopifnot(length(strsplit(countQuery, ";")[[1]]) == 1)
         self$countQuery <- countQuery
       } else {
-        self$countQuery <-  SqlRender::render("SELECT COUNT(*) as count FROM (@sub_query);",
-                                              sub_query = self$baseQuery)
+        self$countQuery <-  SqlRender::render("SELECT COUNT(*) as count FROM (@sub_query);", sub_query = self$baseQuery)
       }
-
-      self
     },
 
     #' get column defs
@@ -85,7 +83,7 @@ LargeDataTable <- R6::R6Class(
     getPage = function(pageNum, pageSize = self$pageSize, ...) {
       mainQuery <- SqlRender::render(sql = self$baseQuery, ...)
 
-      pageOffset <- ((pageNum - 1) * pageSize) + 1
+      pageOffset <- ((pageNum - 1) * pageSize)
       self$connectionHandler$queryDb("@main_query LIMIT @page_size OFFSET @page_offset",
                                      main_query = mainQuery,
                                      page_size = pageSize,
@@ -112,33 +110,51 @@ LargeDataTable <- R6::R6Class(
 #'
 #' @param id    Shiny module id. Must match largeTableServer
 #' @param pageSizeChoices    numeric selection options for pages
-largeTableView <- function(id, pageSizeChoices = c(10,20,50,100)) {
+#' @param selectedPageSize   numeric selection options for pages
+#' @param fullDownloads     allow download button of full dataset from query
+largeTableView <- function(id, pageSizeChoices = c(10,25,50,100), selectedPageSize = 10, fullDownloads = TRUE) {
   ns <- shiny::NS(id)
-  checkmate::assertNumeric(pageSizeChoices)
+  checkmate::assertNumeric(pageSizeChoices, min.len = 1, finite = TRUE, lower = 1)
+  checkmate::assertTRUE(selectedPageSize %in% pageSizeChoices)
+
+  inlineStyle <- ".inline-tv label{ display: table-cell; text-align: center; vertical-align: middle; padding-right: 1em; }
+                  .inline-tv .selectize-input { min-width:70px;}
+                 .inline-tv .form-group { display: table-row;}"
+
   shiny::div(
     id = ns("display-table"),
-    shiny::fluidRow(
-      shiny::column(
-        width = 9
-      ),
-      shiny::column(
-        width = 3,
-        shiny::selectInput(ns("pageSize"), choices = pageSizeChoices, label = "Page Size")
-      )
+    shiny::tags$head(
+      shiny::tags$style(type = "text/css", inlineStyle)
     ),
+     shiny::fluidRow(
+       if (fullDownloads) {
+         shiny::column(
+           width = 4,
+           shiny::downloadButton(ns("downloadFull"),
+                                 label = "Download (Full)",
+                                 icon = shiny::icon("download"))
+         )
+       }
+     ),
     shinycssloaders::withSpinner(reactable::reactableOutput(ns("tableView"))),
     shiny::fluidRow(
       shiny::column(
-        width = 1,
-        shiny::actionButton(ns("previousButton"), label = "Previous")
+        width = 4,
+        class = "inline-tv",
+        shiny::textOutput(ns("pageNumber")),
+        shiny::selectInput(ns("pageSize"),
+                           choices = pageSizeChoices,
+                           selected = selectedPageSize,
+                           label = "show",
+                           width = "90px")
       ),
       shiny::column(
-        width = 1,
-        shiny::actionButton(ns("nextButton"), label = "Next")
+        width = 2
       ),
       shiny::column(
-        width = 3,
-        shiny::p(shiny::textOutput(ns("pageNumber")))
+        width = 6,
+        style = "text-align:right;",
+        shiny::uiOutput(ns("pageActionButtons"))
       )
     )
   )
@@ -158,24 +174,46 @@ largeTableServer <- function(id,
                              inputParams,
                              modifyData = NULL) {
   checkmate::assertR6(ldt, "LargeDataTable")
-  checkmate::assertClass(inputParams, "reactive")
+
+  if (!is.list(inputParams))
+    checkmate::assertClass(inputParams, "reactive")
+
   shiny::moduleServer(id, function(input, output, session) {
+
+    if (is.list(inputParams)) {
+      realParams <- inputParams
+      inputParams <- shiny::reactive(realParams)
+    }
+
+    ns <- session$ns
     pageNum <- shiny::reactiveVal(1)
     pageSize <- shiny::reactive(as.integer(input$pageSize))
 
-    pageCount <- shiny::reactive({
-      count <- do.call(ldt$getCount, inputParams())
-      ceiling(count/pageSize())
+    rowCount <- shiny::reactive({
+      do.call(ldt$getCount, inputParams())
     })
 
-    shiny::observeEvent(input$nextButton, pageNum(min(pageNum() + 1, pageCount())))
+    pageCount <- shiny::reactive({
+      ceiling(rowCount()/pageSize())
+    })
+
+    shiny::observeEvent(input$nextButton, pageNum(min(pageNum() + 1, rowCount())))
     shiny::observeEvent(input$previousButton, pageNum(max(pageNum() - 1, 1)))
+
+    shiny::observeEvent(input$pageNum, pageNum(input$pageNum))
+
     # Reset page on page size change
     shiny::observeEvent(input$pageSize, { pageNum(1) })
 
-    output$pageNumber <- shiny::renderText(paste("Page", pageNum(), "of", format(pageCount(), big.mark = ",")))
+    output$pageNumber <- shiny::renderText({
 
-    output$tableView <- reactable::renderReactable({
+      minNum <- format(((pageNum() - 1) * pageSize()) + 1, big.mark = ",", scientific = FALSE)
+      maxNum <- format((pageNum() - 1)  * pageSize() + pageSize(), big.mark = ",", scientific = FALSE)
+      rc <- format(rowCount(), big.mark = ",", scientific = FALSE)
+      paste(minNum, "-", maxNum, "of", rc, "rows")
+    })
+
+    dataPage <- shiny::reactive({
       params <- inputParams()
       checkmate::assertList(params)
       params$pageNum <- pageNum()
@@ -186,12 +224,63 @@ largeTableServer <- function(id,
       if (is.function(modifyData)) {
         dataPage <- dataPage %>% modifyData(pageNum(), pageSize())
       }
+      dataPage
+    })
 
-      reactable::reactable(dataPage,
+    output$tableView <- reactable::renderReactable({
+      reactable::reactable(dataPage(),
                            columns = ldt$getColumnDefs(),
                            searchable = FALSE,
                            sortable = FALSE,
+                           resizable = TRUE,
+                           outlined = TRUE,
+                           showSortIcon = TRUE,
+                           striped = TRUE,
+                           highlight = TRUE,
+                           defaultColDef = reactable::colDef(align = "left"),
                            pagination = FALSE)
     })
+
+    output$pageActionButtons <-  shiny::renderUI({
+      pc <- pageCount()
+      if (pc == 1) {
+        return(shiny::span(style="width:80px;", shiny::HTML("&nbsp;")))
+      }
+
+      createPageLink <- function(pageLink) {
+        js <- sprintf("Shiny.setInputValue('%s', %s);", ns("pageNum"), pageLink)
+        shiny::tags$a(onclick = js, style = "cursor:pointer;", format(pageLink, big.mark = ",", scientific = FALSE))
+      }
+
+      linkNums <- unique(c(1, max(2, pageNum() - 2):min(pageCount() - 1, pageNum() + 3)))
+      links <- lapply(linkNums, createPageLink)
+
+      ## render action buttons
+      # Always show 1
+      # Show row up to 5
+      # Always show max
+      shiny::tagList(
+        shiny::actionLink(ns("previousButton"), label = "Previous"),
+        links,
+        if (pageCount() != pageNum()) {
+          shiny::span("...")
+        },
+        createPageLink(pageCount()),
+        shiny::actionLink(ns("nextButton"), label = "Next")
+      )
+    })
+
+    output$downloadFull <- shiny::downloadHandler(
+      filename = function() {
+        paste0('result-data-full-', id, Sys.Date(), '.csv')
+      },
+      content = function(con) {
+        shiny::withProgress(
+          message = "preparing download",
+          value = 15,
+          readr::write_csv(do.call(ldt$getAllResults, inputParams()), con)
+        )
+      }
+    )
   })
 }
