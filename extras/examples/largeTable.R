aa45ca6d17b6a489278d678ae22c25e89e5f5331
Jamie Gilbert <JGilber2@its.jnj.com>
2023-07-07 13:28:35 -0700

    Tests and fully operational tables

--- extras/examples/largeTable.R
+++ extras/examples/largeTable.R
@@ -6,7 +6,7 @@
 #
 #    http://shiny.rstudio.com/
 #
-library(OhdisShinyModules)
+devtools::load_all()
 library(shiny)
 
 ui <- fluidPage(
@@ -17,25 +17,25 @@ ui <- fluidPage(
 
 )
 ###--- Fill in connection details with a real db ---###
-connectionDetails <- DatabaseConnector::createConnectionDetails()
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "sqlite", server = ":memory:")
+ch <- ResultModelManager::ConnectionHandler$new(connectionDetails)
+# 1 million random rows
+bigData <- data.frame(row_id = 1:1e6, value = stats::runif(1e6))
+DatabaseConnector::insertTable(ch$getConnection(), data = bigData, tableName = "big_table")
 
-ch <- ResultModelManager::ConnectionHandler$new(connectionDetails = connectionDetails)
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  # Set to schema you are using
-  testSchema <- "ase_004"
 
-  baseQuery <- "SELECT * FROM @test_schema.cd_temporal_covariate_value"
-  countQuery <- "SELECT count(*) as count FROM @test_schema.cd_temporal_covariate_value"
+  baseQuery <- "SELECT * FROM main.big_table WHERE row_id >= @min_row"
+  countQuery <- "SELECT count(*) FROM main.big_table WHERE row_id >= @min_row"
 
   ldt <- LargeDataTable$new(ch,
                             baseQuery,
                             countQuery = countQuery,
                             columnDefs = list(
-                              "cohortId" = reactable::colDef(name = "cohort id")
+                              "rowId" = reactable::colDef(name = "row id")
                             ))
 
-  largeTableServer("tblView", ldt, reactive(list(test_schema = testSchema)))
+  largeTableServer("tblView", ldt, reactive(list(min_row = 1)))
 }
 
 # Run the application
---
+++ tests/testthat/test-components-largeTableViewer.R
@@ -0,0 +1,74 @@
+# Create Tables
+test_that("Large Data Table R6 Class works", {
+  # Create connection handler
+  cdTest <- DatabaseConnector::createConnectionDetails(dbms = "sqlite", server = ":memory:")
+  ch <- ResultModelManager::ConnectionHandler$new(cdTest)
+  on.exit(ch$finalize())
+  # 1 million random rows
+  bigData <- data.frame(row_id = 1:1e6, value = stats::runif(1e6))
+  DatabaseConnector::insertTable(ch$getConnection(), data = bigData, tableName = "big_table")
+
+  query <- "SELECT * FROM main.big_table WHERE row_id >= @min_row"
+  countQuery <- "SELECT count(*) FROM main.big_table WHERE row_id >= @min_row"
+
+  ldt <- LargeDataTable$new(ch, query, countQuery, columnDefs = list(row_id = reactable::colDef("row id")))
+
+  checkmate::expect_r6(ldt, "LargeDataTable")
+
+  expect_equal(ldt$getAllResults(min_row = 1) %>% nrow(), bigData %>% nrow())
+  expect_equal(ldt$getCount(min_row = 2), bigData %>% nrow() - 1)
+  expect_equal(ldt$getCount(min_row = 1), bigData %>% nrow())
+  checkmate::expect_data_frame(ldt$getPage(2, 10, min_row = 1))
+  expect_equal(ldt$getPage(2, 10, min_row = 1) %>% nrow(), 10)
+  checkmate::expect_list(ldt$getColumnDefs())
+  expect_error(LargeDataTable$new(ch, "SELECT 1; SELECT 2;"))
+  expect_error(LargeDataTable$new(ch, query, "SELECT 1; SELECT 2;"))
+  expect_error(LargeDataTable$new(ch, query, columnDefs = list(1, 2, 3)))
+
+  ldt2 <- LargeDataTable$new(ch, query, countQuery = NULL, columnDefs = list(row_id = reactable::colDef("row id")))
+  checkmate::expect_string(ldt2$countQuery)
+
+  checkmate::expect_class(largeTableView("foo"), "shiny.tag")
+  checkmate::expect_class(largeTableView("foo", fullDownloads = FALSE), "shiny.tag")
+
+  shiny::testServer(
+    app = largeTableServer,
+    args = list(
+      id = "foo",
+      ldt = ldt,
+      inputParams = list(min_row = 1),
+      modifyData = function(df, pageNum, pageSize) {
+        df %>% dplyr::mutate(fizzBuzz = ifelse(.data$rowId %% 3 == 0, "fizz", "buzz"))
+      }
+    ),
+    expr = {
+
+      session$setInputs(pageSize = 10)
+
+      expect_equal(rowCount(), ldt$getCount(min_row = 1))
+      expect_equal(pageNum(), 1)
+      expect_equal(pageCount(), ceiling(ldt$getCount(min_row = 1) / 10))
+
+
+      session$setInputs(pageSize = 100)
+      expect_equal(pageNum(), 1)
+      expect_equal(pageCount(), ceiling(ldt$getCount(min_row = 1) / 100))
+
+      session$setInputs(pageNum = 5)
+      expect_equal(pageNum(), 5)
+      expect_equal(output$pageNumber, "401 - 500 of 1,000,000 rows")
+
+      checkmate::expect_data_frame(dataPage(), nrow = 100, ncols = 3)
+      checkmate::expect_names(colnames(dataPage()), must.include = c("rowId", "value", "fizzBuzz"))
+      expect_true(all(dataPage()$fizzBuzz == ifelse(dataPage()$rowId %% 3 == 0, "fizz", "buzz")))
+
+      checkmate::expect_class(output$tableView, "json")
+      checkmate::expect_class(output$pageActionButtons , "list")
+
+      session$setInputs(pageSize = 1e6)
+      expect_equal(pageCount(), 1)
+      checkmate::expect_class(output$pageActionButtons , "list")
+      checkmate::expect_file_exists(output$downloadFull)
+    }
+  )
+})
