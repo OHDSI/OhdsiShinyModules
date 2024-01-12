d4d9b7a2f14b30ab2e38bac21e9c22d2a7db0b13
Jamie Gilbert <JGilber2@its.jnj.com>
2024-01-12 11:44:07 -0800

    Package maintenance

--- R/cohort-method-attrition.R
+++ R/cohort-method-attrition.R
@@ -1,6 +1,6 @@
 # @file cohort-method-attrition
 #
-# Copyright 2023 Observational Health Data Sciences and Informatics
+# Copyright 2024 Observational Health Data Sciences and Informatics
 #
 # This file is part of OhdsiShinyModules
 #
