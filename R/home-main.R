b68f93e5e8fad4f45549425b86d5809cb3530d81
nhall6 <nhall6@its.jnj.com>
2024-03-12 08:51:57 -0400

    Updating home to have initial t, o, and tar selectors

--- R/home-main.R
+++ R/home-main.R
@@ -28,11 +28,13 @@ homeViewer <- function(id=1) {
   
   screens <- list(
     shinyglide::screen(
-      shiny::p("Assure Executive Summary"),
-      shiny::p("Study Name"),
-      shiny::p("The study question was..."),
-      shiny::p("<list available results>"),
-      next_label="Estimation Results"
+        shiny::p("Assure Executive Summary"),
+        shiny::p("Study Name"),
+        shiny::p("The study question was..."),
+        next_label="Next: Characterization Results",
+        inputSelectionViewer(
+          id = ns("input-selection-results")
+        )
     ),
     shinyglide::screen(
       shiny::p("Estimation Results"),
@@ -57,7 +59,7 @@ homeViewer <- function(id=1) {
     solidHeader = TRUE,
     
     shinyglide::glide(
-      height = "350px",
+      height = "500px",
       screens
     )
     
@@ -82,11 +84,196 @@ homeViewer <- function(id=1) {
 homeServer <- function(
     id, 
     connectionHandler,
-    resultDatabaseSettings = list(port = 1)
+    resultDatabaseSettings
 ) {
   shiny::moduleServer(
     id,
     function(input, output, session) {
+      
+      #get target, outcome, and TAR options
+      options <- getExecutiveSummaryOptions(
+        connectionHandler,
+        resultDatabaseSettings
+      )
+      
+      # Extract the integers from each TAR string
+      tarIntegers <- as.integer(gsub("[^0-9]", "", options$tar))
+      # Sort the vector based on the extracted integers
+      sortedTars <- options$tar[order(tarIntegers)]
+      
+      
+      # input selection component
+      inputSelectedResults <- inputSelectionServer(
+        id = "input-selection-results", 
+        inputSettingList = list(
+          createInputSetting(
+            rowNumber = 1,                           
+            columnWidth = 12,
+            varName = 'firsttext',
+            inputReturn = T,
+            uiFunction = 'shiny::div',
+            uiInputs = list(
+              "Select One of Each: Target, Outcome, and Time at Risk (TAR)",
+              style = "font-weight: bold; font-size: 20px; text-align: center; margin-bottom: 20px;"
+            )
+          ),
+          createInputSetting(
+            rowNumber = 2,                           
+            columnWidth = 4,
+            varName = 'targetIds',
+            uiFunction = 'shinyWidgets::pickerInput',
+            updateFunction = 'shinyWidgets::updatePickerInput',
+            uiInputs = list(
+              label = 'Target: ',
+              choices = options$targetIds,
+              selected = options$targetIds[1], #default should be just one (the first)
+              multiple = F,
+              options = shinyWidgets::pickerOptions(
+                actionsBox = TRUE,
+                liveSearch = TRUE,
+                size = 10,
+                liveSearchStyle = "contains",
+                liveSearchPlaceholder = "Type here to search",
+                virtualScroll = 50
+              )
+            )
+          ),
+          createInputSetting(
+            rowNumber = 2,                           
+            columnWidth = 4,
+            varName = 'outcomeIds',
+            uiFunction = 'shinyWidgets::pickerInput',
+            updateFunction = 'shinyWidgets::updatePickerInput',
+            uiInputs = list(
+              label = 'Outcome: ',
+              choices = options$outcomeIds,
+              selected = options$outcomeIds[1], #default should be just one (the first)
+              multiple = F,
+              options = shinyWidgets::pickerOptions(
+                actionsBox = TRUE,
+                liveSearch = TRUE,
+                size = 10,
+                liveSearchStyle = "contains",
+                liveSearchPlaceholder = "Type here to search",
+                virtualScroll = 50
+              )
+            )
+          ),
+          
+          createInputSetting(
+            rowNumber = 2,                           
+            columnWidth = 4,
+            varName = 'tarFilter',
+            uiFunction = 'shinyWidgets::pickerInput',
+            updateFunction = 'shinyWidgets::updatePickerInput',
+            uiInputs = list(
+              label = 'Select Time at Risk (TAR)',
+              choices = sortedTars,
+              selected = sortedTars[1],
+              multiple = F,
+              options = shinyWidgets::pickerOptions(
+                actionsBox = TRUE,
+                liveSearch = TRUE,
+                size = 10,
+                dropupAuto = TRUE,
+                liveSearchStyle = "contains",
+                liveSearchPlaceholder = "Type here to search",
+                virtualScroll = 50
+              )
+            )
+          )
+        )
+      )
+      
+      
+      
+     #-------Characterization executive summary
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      getExecutiveSummaryOptions <- function(
+    connectionHandler,
+    resultDatabaseSettings
+      ){
+        
+        # shiny::withProgress(message = 'Getting incidence inputs', value = 0, {
+        
+        sql <- 'select distinct target_cohort_definition_id, target_name 
+  from @result_schema.@incidence_table_prefixINCIDENCE_SUMMARY;'
+        
+        #shiny::incProgress(1/3, detail = paste("Created SQL - Extracting targets"))
+        
+        targets <- connectionHandler$queryDb(
+          sql = sql, 
+          result_schema = resultDatabaseSettings$schema,
+          incidence_table_prefix = resultDatabaseSettings$incidenceTablePrefix
+        )
+        targetIds <- targets$targetCohortDefinitionId
+        names(targetIds) <- targets$targetName
+        
+        sql <- 'select distinct outcome_cohort_definition_id, outcome_name 
+  from @result_schema.@incidence_table_prefixINCIDENCE_SUMMARY;'
+        
+        #shiny::incProgress(2/3, detail = paste("Created SQL - Extracting outcomes"))
+        
+        outcomes <- connectionHandler$queryDb(
+          sql = sql, 
+          result_schema = resultDatabaseSettings$schema,
+          incidence_table_prefix = resultDatabaseSettings$incidenceTablePrefix
+        )
+        
+        outcomeIds <- outcomes$outcomeCohortDefinitionId
+        names(outcomeIds) <- outcomes$outcomeName
+        
+        sql <- 'select distinct tar_id, tar_start_with, tar_start_offset, tar_end_with, tar_end_offset
+  from @result_schema.@incidence_table_prefixINCIDENCE_SUMMARY;'
+        
+        #shiny::incProgress(1/3, detail = paste("Created SQL - Extracting targets"))
+        
+        tars <- connectionHandler$queryDb(
+          sql = sql, 
+          result_schema = resultDatabaseSettings$schema,
+          incidence_table_prefix = resultDatabaseSettings$incidenceTablePrefix
+        )
+        tar <- paste0('(',tars$tarStartWith, " + ", tars$tarStartOffset, ') - (', tars$tarEndWith, " + ", tars$tarEndOffset, ')')
+        #tar <- tars$tarId
+        names(tar) <- paste0('(',tars$tarStartWith, " + ", tars$tarStartOffset, ') - (', tars$tarEndWith, " + ", tars$tarEndOffset, ')')
+        
+        categoricalChoices <- list(
+          "targetName",
+          "outcomeName",
+          "tar"
+        )
+        names(categoricalChoices) <- c(
+          "Target Cohort",
+          "Outcome Cohort", 
+          "TAR"
+        )
+        
+        return(
+          list(
+            targetIds = targetIds,
+            outcomeIds = outcomeIds,
+            tar = tar,
+            categoricalChoices = categoricalChoices
+          )
+        )
+        
+      }
+      
+      
+      
+      
       
       output$cool_plot <- shiny::renderPlot({
         graphics::hist(
