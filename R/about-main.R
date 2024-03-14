7618a11fe97909bf510c2ee14f68650088545b58
nhall6 <nhall6@its.jnj.com>
2024-03-14 10:27:36 -0400

    updating about module

--- R/about-main.R
+++ R/about-main.R
@@ -47,24 +47,288 @@ aboutViewer <- function(
     ) {
   ns <- shiny::NS(id)
   
-  shinydashboard::box(
-    status = 'info', width = 12,
-    title =  shiny::span( shiny::icon("info"), "About OHDSI Viewer"),
-    solidHeader = TRUE,
+  #shinydashboard::dashboardBody(
+    shiny::div(
+    fluidRow(
+      tags$head(tags$style(HTML(".small-box {height: 200px; width: 100%;}"))),
+      shinydashboard::box(
+        width = "100%",
+        shiny::htmlTemplate(system.file("about-www", "about.html", package = utils::packageName()))
+      )
+   # )
+  ),
+  fluidRow(
+    valueBoxOutput("newOrdersBox", width = 3),
+    valueBoxOutput("progressBox", width = 3),
+    valueBoxOutput("approvalBox", width = 3),
+    valueBoxOutput("valueBox1", width = 3)
+  ),
+  fluidRow(
+    valueBoxOutput("valueBox2", width = 3),
+    valueBoxOutput("valueBox3", width = 3),
+    valueBoxOutput("valueBox4", width = 3),
+    valueBoxOutput("valueBox5", width = 3)
+  ),
+  tags$script('
+      $(document).ready(function(){
+        $("#newOrdersBox").click(function(){
+          $("#newOrdersModal").modal("show");
+        });
 
-    shiny::fluidPage(
-      shiny::fluidRow(
-        shiny::includeMarkdown(
-          path = system.file(
-            'about-document', 
-            "introduction.md", 
-            package = "OhdsiShinyModules"
+        $("#progressBox").click(function(){
+          $("#progressModal").modal("show");
+        });
+
+        $("#approvalBox").click(function(){
+          $("#approvalModal").modal("show");
+        });
+
+        $("#valueBox1").click(function(){
+          $("#modal1").modal("show");
+        });
+
+        $("#valueBox2").click(function(){
+          $("#modal2").modal("show");
+        });
+
+        $("#valueBox3").click(function(){
+          $("#modal3").modal("show");
+        });
+
+        $("#valueBox4").click(function(){
+          $("#modal4").modal("show");
+        });
+
+        $("#valueBox5").click(function(){
+          $("#modal5").modal("show");
+        });
+      });
+    '),
+  tags$div(
+    id = "newOrdersModal",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "New Orders Modal"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
           )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is the New Orders modal."
         )
       )
     )
-    
+  ),
+  tags$div(
+    id = "progressModal",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Progress Modal"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is the Progress modal."
+        )
+      )
+    )
+  ),
+  tags$div(
+    id = "approvalModal",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Approval Modal"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is the Approval modal."
+        )
+      )
+    )
+  ),
+  tags$div(
+    id = "modal1",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Modal 1"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is Modal 1."
+        )
+      )
+    )
+  ),
+  tags$div(
+    id = "modal2",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Modal 2"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is Modal 2."
+        )
+      )
+    )
+  ),
+  tags$div(
+    id = "modal3",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Modal 3"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is Modal 3."
+        )
+      )
+    )
+  ),
+  tags$div(
+    id = "modal4",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Modal 4"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is Modal 4."
+        )
+      )
+    )
+  ),
+  tags$div(
+    id = "modal5",
+    class = "modal fade",
+    tags$div(
+      class = "modal-dialog modal-dialog-centered",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class = "modal-header",
+          tags$h3(
+            class = "modal-title",
+            "Modal 5"
+          ),
+          tags$button(
+            type = "button",
+            class = "close",
+            `data-dismiss` = "modal",
+            `aria-label` = "Close",
+            tags$span(`aria-hidden` = "true", "×")
+          )
+        ),
+        tags$div(
+          class = "modal-body",
+          "This is Modal 5."
+        )
+      )
+    )
   )
+    )
 }
 
 #' The module server for the shiny app home
@@ -75,6 +339,7 @@ aboutViewer <- function(
 #' @param id  the unique reference id for the module
 #' @param connectionHandler a connection to the database with the results
 #' @param resultDatabaseSettings a list containing the characterization result schema, dbms, tablePrefix, databaseTable and cgTablePrefix
+#' @param config the config from the app.R file that contains a list of which modules to include
 #' 
 #' @return
 #' The server for the shiny app home
@@ -83,11 +348,93 @@ aboutViewer <- function(
 aboutServer <- function(
     id = 'homepage',
     connectionHandler = NULL,
-    resultDatabaseSettings = NULL
+    resultDatabaseSettings = NULL,
+    config
     ) {
   shiny::moduleServer(
     id,
     function(input, output, session) {
+      
+      tab_names <- character()
+      # Loop through shinyModules and extract tabName values
+      for (i in seq_along(config[["shinyModules"]])) {
+        tab_name <- config[["shinyModules"]][[i]][["tabName"]]
+        tab_names <- c(tab_names, tab_name)
+      }
+      # View the extracted tabName values
+      # print(tab_names)
+
+      output$newOrdersBox <- shinydashboard::renderValueBox({
+      if ("DataSources" %in% tab_names) {
+        shinydashboard::valueBox(
+          "Data Sources", "Databases used in this analysis", icon = shiny::icon("database"),
+          color = "purple",
+          href = "#newOrdersModal"
+        )
+      } else {
+        shinydashboard::valueBox(
+          "Data Sources", "This module was not included in this analysis.", icon = shiny::icon("database"),
+          color = "black",
+          href = "#newOrdersModal"
+        )
+      }
+      })
+      
+      output$progressBox <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "100%", "Progress", icon = shiny::icon("list"),
+          color = "black",
+          href = "#progressModal"
+        )
+      })
+      
+      output$approvalBox <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "80%", "Approval", icon = shiny::icon("thumbs-up", lib = "glyphicon"),
+          color = "yellow",
+          href = "#approvalModal"
+        )
+      })
+      
+      output$valueBox1 <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "Text 1", "Value Box 1", icon = shiny::icon("star"),
+          color = "blue",
+          href = "#modal1"
+        )
+      })
+      
+      output$valueBox2 <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "Text 2", "Value Box 2", icon = shiny::icon("heart"),
+          color = "maroon",
+          href = "#modal2"
+        )
+      })
+      
+      output$valueBox3 <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "Text 3", "Value Box 3", icon = shiny::icon("globe"),
+          color = "green",
+          href = "#modal3"
+        )
+      })
+      
+      output$valueBox4 <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "Text 4", "Value Box 4", icon = shiny::icon("car"),
+          color = "orange",
+          href = "#modal4"
+        )
+      })
+      
+      output$valueBox5 <- shinydashboard::renderValueBox({
+        shinydashboard::valueBox(
+          "Text 5", "Value Box 5", icon = shiny::icon("car"),
+          color = "aqua",
+          href = "#modal4"
+        )
+      })
       
       
     }
