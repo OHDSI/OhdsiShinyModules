a7b6685afcd1380cb11694def237087b70bd0031
Anthony Sena <asena5@its.jnj.com>
2024-07-30 16:21:58 -0400

    Fixes estimation bug when target has no comparator for CM

--- R/estimation-cohort-method-diagnostics.R
+++ R/estimation-cohort-method-diagnostics.R
@@ -92,6 +92,7 @@ estimationGetCmDiagnostics <- function(
       {@use_analyses}?{and cma.analysis_id in (@analyses)}
       ;
   "
+  print(comparatorIds)
   
   result <- connectionHandler$queryDb(
     sql = sql,
@@ -104,7 +105,7 @@ estimationGetCmDiagnostics <- function(
     comparators = paste0(comparatorIds, collapse = ','),
     outcomes = paste0(outcomeId, collapse = ','),
     
-    use_comparators = T,
+    use_comparators = ifelse(is.null(comparatorIds), F, T),
     use_analyses = F
   )
   
