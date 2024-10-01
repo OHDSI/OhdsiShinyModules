713ee3c827de6e42b16a3b42438ac59344188d84
Nathan Hall <nhall6@its.jnj.com>
2024-10-01 13:54:51 -0400

    adding plots to db and cohort comparison
    adding scatterplots to characterization db and cohort compare tabs to address #294

--- R/characterization-database.R
+++ R/characterization-database.R
@@ -48,7 +48,21 @@ characterizationDatabaseComparisonViewer <- function(id) {
         ),
         shiny::tabPanel(
           title = 'Binary',
-          resultTableViewer(id = ns('mainTable'), boxTitle = 'Binary')
+          shiny::tabsetPanel(
+            type = 'pills',
+            id = ns('binaryPanel'),
+            shiny::tabPanel(
+              title = "Table",
+              resultTableViewer(id = ns('mainTable'), boxTitle = 'Binary')
+            ),
+            shiny::tabPanel(
+              title = "Plot",
+              shiny::uiOutput(ns('plotInputs')),
+              shinycssloaders::withSpinner(
+                plotly::plotlyOutput(ns('scatterPlot'))
+              )
+            )
+          )
         ),
         shiny::tabPanel(
           title = 'Continuous',
@@ -277,6 +291,208 @@ characterizationDatabaseComparisonServer <- function(
             ),
             elementId = session$ns('continuous-table-filter')
           )
+          
+          
+          #scatterplots
+          
+          plotResult <- characterizatonGetDatabaseComparisonDataRaw(
+            connectionHandler = connectionHandler,
+            resultDatabaseSettings = resultDatabaseSettings,
+            targetIds = subTargetId(),
+            databaseIds = input$databaseIds,
+            minThreshold = input$minThreshold
+          )
+          
+          names(plotResult$databaseId) <- plotResult$cdmSourceAbbreviation
+          
+          output$plotInputs <- shiny::renderUI({
+            shiny::div(
+              shiny::fluidRow(
+                shiny::column(width = 5,
+                              shinyWidgets::pickerInput(
+                                inputId = session$ns('xAxis'), 
+                                label = 'X-Axis Database: ',
+                                choices = unique(plotResult$cdmSourceAbbreviation),
+                                selected = unique(plotResult$cdmSourceAbbreviation)[1],
+                                multiple = F,
+                                options = shinyWidgets::pickerOptions(
+                                  actionsBox = TRUE,
+                                  liveSearch = TRUE,
+                                  size = 10,
+                                  dropupAuto = TRUE,
+                                  liveSearchStyle = "contains",
+                                  liveSearchPlaceholder = "Type here to search",
+                                  virtualScroll = 50
+                                )
+                              )
+                ),
+                shiny::column(width = 5,
+                              shinyWidgets::pickerInput(
+                                inputId = session$ns('yAxis'), 
+                                label = 'Y-Axis Database: ',
+                                choices = unique(plotResult$cdmSourceAbbreviation),
+                                selected = unique(plotResult$cdmSourceAbbreviation)[2],
+                                multiple = F,
+                                options = shinyWidgets::pickerOptions(
+                                  actionsBox = TRUE,
+                                  liveSearch = TRUE,
+                                  size = 10,
+                                  dropupAuto = TRUE,
+                                  liveSearchStyle = "contains",
+                                  liveSearchPlaceholder = "Type here to search",
+                                  virtualScroll = 50
+                                )
+                              )
+                )
+              ),
+              shiny::fluidRow(
+                shiny::column(
+                  width = 4,
+                  shiny::actionButton(
+                  inputId = session$ns('generatePlot'), 
+                  label = 'Generate Plot'
+                  )
+                )
+              )
+            )
+          })
+          
+          #get results
+          selectedPlotDbs <- shiny::reactiveVal()
+          shiny::observeEvent(input$generatePlot,{
+            
+            plotDf <- shiny::reactive({
+              
+              # Filter the plot result based on selected xAxis and yAxis inputs
+              plotResult2 <- plotResult %>%
+                dplyr::filter(.data$cdmSourceAbbreviation %in% c(input$xAxis, input$yAxis))
+              
+              # Group and split the data by cdmSourceAbbreviation
+              plotResultDbSplit <- plotResult2 %>%
+                dplyr::group_by(.data$cdmSourceAbbreviation) %>%
+                dplyr::group_split()
+              
+              # Initialize an empty list to store the processed dataframes
+              processedDfs <- list()
+              
+              # Loop over the split datasets and process each one
+              for (i in seq_along(plotResultDbSplit)) {
+                
+                currentDb <- plotResultDbSplit[[i]]
+                
+                currentDbDf <- currentDb %>%
+                  dplyr::select(cdmSourceAbbreviation,
+                                covariateName,
+                                averageValue)
+                
+                # Ensure only rows with selected xAxis or yAxis inputs are kept
+                currentDbDf <- currentDbDf %>%
+                  dplyr::filter(.data$cdmSourceAbbreviation %in% c(input$xAxis, input$yAxis))
+                
+                # Get the name for this database (should be unique after filtering)
+                dbName <- unique(currentDbDf$cdmSourceAbbreviation)
+                
+                # Rename the averageValue column based on the database name
+                colnames(currentDbDf) <- c("cdmSourceAbbreviation", "covariateName", paste0(dbName, "_avg"))
+                
+                # Remove the cdmSourceAbbreviation column for joining later
+                currentDbDf <- currentDbDf %>%
+                  dplyr::select(-cdmSourceAbbreviation)
+                
+                # Append the processed dataframe to the list
+                processedDfs[[i]] <- currentDbDf
+              }
+              
+              # Check if there's at least one dataframe to join
+              if (length(processedDfs) > 1) {
+                # Perform a left join across all processed dataframes
+                plotResultDbComb <- Reduce(function(x, y) dplyr::left_join(x, y, by = "covariateName"), processedDfs)
+              } else {
+                # If there's only one dataframe, no need for joining
+                plotResultDbComb <- processedDfs[[1]]
+              }
+              
+              # Replace NA values with 0
+              plotResultDbComb <- plotResultDbComb %>%
+                replace(is.na(.), 0) %>%
+                dplyr::mutate(domain = dplyr::case_when(
+                  grepl("condition_", covariateName) | sub("\\s.*", "", covariateName) == "condition" ~ "Condition",
+                  grepl("drug_", covariateName) | sub("\\s.*", "", covariateName) == "drug" ~ "Drug",
+                  grepl("procedure_", covariateName) | sub("\\s.*", "", covariateName) == "procedure" ~ "Procedure",
+                  grepl("measurement_", covariateName) | sub("\\s.*", "", covariateName) == "measurement" ~ "Measurement",
+                  grepl("observation_", covariateName) | sub("\\s.*", "", covariateName) == "observation" ~ "Observation",
+                  grepl("device_", covariateName) | sub("\\s.*", "", covariateName) == "device" ~ "Device",
+                  grepl("cohort_", covariateName) | sub("\\s.*", "", covariateName) == "cohort" ~ "Cohort",
+                  grepl("visit_", covariateName) | sub("\\s.*", "", covariateName) == "visit" ~ "Visit",
+                  .default = "Demographic"
+                ))
+              
+              return(plotResultDbComb)
+              
+              
+              
+            })
+          
+          #plot
+          
+          shiny::observe({
+            output$scatterPlot <- plotly::renderPlotly({
+              
+              # Get the filtered and processed plot data
+              plotData <- plotDf()
+              
+              # Ensure that the reactive inputs are valid and accessible
+              xAxisInput <- input$xAxis
+              yAxisInput <- input$yAxis
+              
+              # Sanitize the xAxis and yAxis input values by replacing spaces with underscores
+              xAxisSafe <- gsub(" ", "_", xAxisInput)
+              yAxisSafe <- gsub(" ", "_", yAxisInput)
+              
+              # Sanitize column names in plotData to replace spaces with underscores
+              colnames(plotData) <- gsub(" ", "_", colnames(plotData))
+              
+              # Ensure that the column names exist in plotData
+              if (!all(c(paste0(xAxisSafe, "_avg"), paste0(yAxisSafe, "_avg")) %in% colnames(plotData))) {
+                stop("Selected columns not found in data.")
+              }
+              
+              # Create hover text for plotly
+              plotData$hoverText <- paste(
+                "Covariate Name:", plotData$covariateName, 
+                "<br>", xAxisInput, ":", scales::percent(plotData[[paste0(xAxisSafe, "_avg")]]), 
+                "<br>", yAxisInput, ":", scales::percent(plotData[[paste0(yAxisSafe, "_avg")]])
+              )
+              
+              # Create the scatter plot with the diagonal line (x = y)
+              p <- ggplot2::ggplot(plotData, ggplot2::aes_string(x = paste0(xAxisSafe, "_avg"),
+                                                                 y = paste0(yAxisSafe, "_avg"),
+                                                                 color = "domain",
+                                                                 text = "hoverText")) +  # Use hoverText for hover labels
+                ggplot2::geom_point(size = 2) +    # Smaller point size
+                ggplot2::geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "black") +  # Diagonal x=y line in black
+                ggplot2::labs(
+                  x = paste0(xAxisInput, " %"),
+                  y = paste0(yAxisInput, " %"),
+                  color = "Domain"
+                ) +
+                ggplot2::theme_minimal() +          # Optional: use a clean theme
+                ggplot2::theme(
+                  legend.position = "right",        # Position legend as needed
+                  axis.title = ggplot2::element_text(size = 12),  # Adjust axis title size
+                  axis.text = ggplot2::element_text(size = 10)    # Adjust axis text size
+                ) +
+                ggplot2::scale_x_continuous(labels = scales::percent_format()) +  # Format x-axis as percentage
+                ggplot2::scale_y_continuous(labels = scales::percent_format())    # Format y-axis as percentage
+              
+              # Convert to a plotly object for interactivity
+              plotly::ggplotly(p, tooltip = "text")  # Use the custom hover text
+            })
+          })
+          
+          
+          })
+          
       })
       
     
@@ -326,4 +542,57 @@ characterizatonGetDatabaseComparisonData <- function(
     )
   )
   
+}
+
+characterizatonGetDatabaseComparisonDataRaw <- function(
+    connectionHandler,
+    resultDatabaseSettings,
+    targetIds,
+    databaseIds,
+    minThreshold = 0.01,
+    addSMD = F
+){
+  
+  if(is.null(targetIds) |  is.null(databaseIds)){
+    warning('Ids cannot be NULL')
+    return(NULL)
+  }
+  
+  sql <- "select  d.cdm_source_abbreviation,
+          ref.covariate_name, 
+          s.min_prior_observation,
+          cov.target_cohort_id as cohort_definition_id,
+          cov.* from   
+    @schema.@c_table_prefixCOVARIATES cov 
+    inner join 
+    @schema.@c_table_prefixcovariate_ref ref
+    on cov.covariate_id = ref.covariate_id
+    and cov.setting_id = ref.setting_id
+    and cov.database_id = ref.database_id
+    inner join 
+    @schema.@c_table_prefixsettings s
+    on s.database_id = cov.database_id
+    and s.setting_id = cov.setting_id
+    inner join 
+    @schema.@database_table d
+    on cov.database_id = d.database_id
+    
+    where 
+    cov.target_cohort_id in (@target_ids) 
+    and cov.cohort_type = 'Target'
+    AND cov.database_id in (@database_ids)
+    AND cov.average_value >= @min_threshold;"
+  
+  # settings.min_characterization_mean needed?
+  res <- connectionHandler$queryDb(
+    sql = sql,
+    target_ids = paste0(targetIds, collapse = ','),
+    database_ids = paste0("'",databaseIds,"'", collapse = ','),
+    schema = resultDatabaseSettings$schema,
+    c_table_prefix = resultDatabaseSettings$cTablePrefix,
+    min_threshold = minThreshold,
+    database_table = resultDatabaseSettings$databaseTable
+  )
+  
+  return(res)
 }
